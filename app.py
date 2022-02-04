e1729a0b696432f42bfc2b423d591dde00b72905
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-02-04 21:53:16 +0100

    Setup database

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask, jsonify, render_template
-
+from flask import Flask, jsonify, render_template, redirect, request
+from contextlib import closing
+from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
+db_name = 'tracks.db'
 
+# ensure table
+with closing(sqlite3.connect("tracks.db")) as connection:
+    connection.execute("CREATE TABLE IF NOT EXISTS tracks (id INTEGER PRIMARY KEY, gpx TEXT, place TEXT, type TEXT, distance INTEGER)")
+
+def select():
+    with closing(sqlite3.connect("tracks.db")) as connection:
+        rows = connection.execute("SELECT * FROM tracks").fetchall()
+        print(f"SELECT {len(rows)} rows")
+        return rows
+
+def insert():
+    with closing(sqlite3.connect("tracks.db")) as connection:
+        print('INSERT')
+        connection.execute("INSERT INTO tracks VALUES (?, '<test>', 'Leiderdorp', 'roundtrip', 34)", (int(round(datetime.now().timestamp())),))
+        connection.commit()
+
+    
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/api/list")
 def list():
-    return jsonify("hallo", "daar")
+    return jsonify(select())
+
+@app.route("/api/create", methods = ['POST'])
+def create():
+    insert()
+    return render_template('index.html')
--- static/tracklist.js
+++ static/tracklist.js
@@ -8,5 +8,19 @@ new Vue({
         const gResponse = await fetch('/api/list');
         const gObject = await gResponse.json();
         this.greeting = gObject;
+    },
+    methods: {
+      createTrack: async function () {
+        console.log('new!')
+
+        const request = {
+          method: "POST" //,
+          //headers: { "Content-Type": "application/json" },
+          //body: JSON.stringify({ title: "Vue POST Request Example" })
+        };
+
+        await fetch('/api/create', request);
+        location.reload();
+      }
     }
 })
