713630327e890823c3710042dda2d1d1f8e6d651
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-02-16 21:41:50 +0100

    Styling and active record

--- static/main.js
+++ static/main.js
@@ -6,9 +6,15 @@ new Vue({
       gpx: 'gpx...'
     },
     created: async function(){
-        const gResponse = await fetch('/api/list');
-        const gObject = await gResponse.json();
-        this.tracks = gObject;
+        const response = await fetch('/api/list');
+        const tracklist = await response.json();
+        
+        tracklist.forEach(x => x.active = false);
+        tracklist[0].active = true;
+
+        this.tracks = tracklist;
+
+        this.viewTrack(tracklist[0]);
     },
     methods: {
       createTrack: async function () {
@@ -23,11 +29,14 @@ new Vue({
         await fetch('/api/create', request);
         location.reload();
       },
-      viewTrack: async function (id) {
-        const gResponse = await fetch('/api/get/' + id);
-        const gObject = await gResponse.json();
+      viewTrack: async function (item) {
+        this.tracks.forEach(x => x.active = false);
+        item.active = true;
 
-        this.gpx = 'clicked ' + id + gObject 
+        const response = await fetch('/api/get/' + item.id);
+        const gpxdata = await response.json();
+
+        this.gpx = 'clicked ' + item.id + gpxdata 
       }
     }
 })
