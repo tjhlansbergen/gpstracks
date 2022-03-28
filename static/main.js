b9c7abc8173e86fa4bce7026a952baa60c0b2d26
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-03-28 21:32:31 +0200

    Fix map

--- static/main.js
+++ static/main.js
@@ -1,9 +1,14 @@
 new Vue({
     el: '#main',
+    components: {
+      'l-map': window.Vue2Leaflet.LMap,
+      'l-tile-layer': window.Vue2Leaflet.LTileLayer
+    },
     delimiters: ['[[', ']]'],
     data: {
       tracks: ['loading...'],
-      gpx: 'gpx...'
+      gpx: 'gpx...',
+      track: null
     },
     created: async function(){
         const response = await fetch('/api/list');
@@ -23,7 +28,27 @@ new Vue({
         const response = await fetch('/api/get/' + item.id);
         const gpxdata = await response.json();
 
-        this.gpx = gpxdata 
+        this.gpx = gpxdata[0]; 
+
+        var map = this.$refs.map.mapObject;
+
+        if(this.track != null) { map.removeLayer(this.track); }
+
+        this.track = new L.GPX(this.gpx, {
+          async: true,
+          marker_options: {
+            startIconUrl: '',
+            endIconUrl: '',
+            shadowUrl: ''
+            },
+          polyline_options: {
+              color: 'magenta',
+              weight: 3,
+              lineCap: 'round'
+            }
+        }).on('loaded', function(e) {
+          map.fitBounds(e.target.getBounds());
+        }).addTo(map);
       }
     }
 })
